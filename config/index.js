958b22b377f88bee4801f6723cc4ac3a9474178e
Jonas Enge <jonas.enge@t-fk.no>
2017-05-10 13:29:56 +0200

    Changes default internal address

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@
 const SSO_IPS = process.env.SSO_IPS ? process.env.SSO_IPS.split(',') : ['139.164.160.2', '213.188.19.179']
 
 module.exports = {
-  AUTH_URL_INTERNAL: process.env.AUTH_URL_INTERNAL || 'http://authportalen',
+  AUTH_URL_INTERNAL: process.env.AUTH_URL_INTERNAL || 'http://tfk-fh-ntlm',
   AUTH_URL_EXTERNAL: process.env.AUTH_URL_EXTERNAL || 'https://auth.service.t-fk.no/login',
   SSO_IPS: SSO_IPS
 }
